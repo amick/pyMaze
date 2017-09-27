94dbb16b5e7f35aab9fa6feb9fa56d7a40ea9189
Andy Amick <aamick@redgradient.com>
2017-09-26 19:24:00 -0600

    create a simple maze with 2d array

---
+++ pyMaze.py
@@ -0,0 +1,26 @@
+from __future__ import print_function
+
+print('Welcome to PyMaze')
+
+# Setup an x by y 2D array full of walls (+)
+col = 5
+row = 4
+mz = [["#"] * col for i in range(row)]
+print("mz = %r" % mz)
+
+# set the open path
+mz[0][1] = " "
+mz[1][1] = " "
+mz[1][2] = " "
+mz[2][2] = " "
+mz[2][3] = " "
+mz[3][3] = " "
+
+# print the maze
+for y in range(row):
+  for x in range(col):
+  	print(mz[y][x], end=' ')
+  print("")
+
+print("")
+print("Goodbye")
